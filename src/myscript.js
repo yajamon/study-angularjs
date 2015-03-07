1d0ab9e4e2fbe0dacd5160b9d4f4db69421a33c0
yajamon <yajamon.tatsuki@gmail.com>
2015-03-07 22:25:52 +0900

    Create data

--- src/myscript.js
+++ src/myscript.js
@@ -1,2 +1,7 @@
 var mainCtrl = function ($scope) {
+    $scope.users = [
+        {"name":"yajamon", "score":55.24 },
+        {"name":"jaamony", "score":34.62 },
+        {"name":"monjaya", "score":23.93 },
+    ];
 }
